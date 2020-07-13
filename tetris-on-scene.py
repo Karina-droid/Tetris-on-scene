fff6512f615d367b863d1152bff36211509adce5
Karina-droid <59184590+Karina-droid@users.noreply.github.com>
2020-07-13 15:55:30 +0300

    Update tetris-on-scene.py

--- tetris-on-scene.py
+++ tetris-on-scene.py
@@ -9,8 +9,7 @@ side = int(rect_w/10)
 colors = ['red', 'orange']
 grounded_blocks = []
 
-NEW = None
-
+new = False
 A = Action
 
 
@@ -36,13 +35,13 @@ class Board(ShapeNode, SpriteNode):
 class Shape(SpriteNode):
 	def __init__(self):
 		
-		self.RIGHT = None
-		self.LEFT = None
+		self.RIGHT = False
+		self.LEFT = False
 		
 		num = random.randrange(0, 2)
 		
 		x = random.randrange(int(-rect_w/2), int(rect_w/2) - num*side, side) + side/2
-		y = rect_h/2
+		y = rect_h/2 - side/2
 		
 		dot =  [[
 				  [x, y]
@@ -69,28 +68,25 @@ class Shape(SpriteNode):
 					
 	def move_down(self):
 		for block in self.shape:
-			if self.if_grounded():
-				for block in self.shape:
-					grounded_blocks.append(block)
-					block.remove_all_actions()
-				self.shape = []
-				global NEW
-				NEW = True
-				break
-			
-			else:
-				block.run_action(A.move_by(0, -side))
-				
+			y = block.position.y - side
+			block.position = Point(block.position.x, y) 
+		
 				
 	def left_right(self):
-		for block in self.shape:
-			if self.RIGHT:
-				x = block.position.x + side
-				block.position = Point(x, block.position.y)
+		if self.RIGHT:
+			right_x = max([b.position.x for b in self.shape])
+			if right_x != 153:
+				for block in self.shape:
+					x = block.position.x + side
+					block.position = Point(x, block.position.y)
 						
-			elif self.LEFT:
-				x = block.position.x - side
-				block.position = Point(x, block.position.y)
+		elif self.LEFT:
+			left_x = min([b.position.x for b in self.shape])
+			if left_x != -153:
+				for block in self.shape:
+					x = block.position.x - side
+					block.position = Point(x, block.position.y)
+	
 	
 	def fall_abruptly(self):
 		pass
@@ -98,11 +94,11 @@ class Shape(SpriteNode):
 	
 	def if_grounded(self):
 		for block in self.shape:
-			if block.position.y < -rect_h/2 + side/2:
+			if block.position.y < -rect_h/2 + side:
 				return True
 			else:
 				for b in grounded_blocks:
-					if (block.position.x == b.position.x) and (block.position.y - side < b.position.y):
+					if (block.position.x == b.position.x) and (block.position.y - side == b.position.y):
 						return True
 
 
@@ -114,17 +110,24 @@ class Game(Scene):
 		
 		self.seconds = 0
 		self.figure = None
-		self.NEW = None
 		self.add_buttons()
 		self.add_figure()
 	
 	
 	def update(self):
 		self.seconds += self.dt
-		if self.seconds > 0.05:
+		if self.seconds > 0.3:
 			self.seconds = 0
-			self.figure.move_down()
-			if NEW:
+			if not self.figure.if_grounded():
+				self.figure.move_down()
+			else:
+				for block in self.figure.shape:
+					grounded_blocks.append(block)
+					self.figure.shape = []
+				global new
+				new = True
+				
+			if new:
 				self.add_figure()
 		
 			
@@ -142,8 +145,6 @@ class Game(Scene):
 				elif 'left' in arw.icon:
 					self.figure.LEFT = True
 					self.figure.left_right()
-					
-				print(self.figure.RIGHT, self.figure.LEFT)
 			
 	
 	def touch_ended(self, touch):
@@ -160,8 +161,8 @@ class Game(Scene):
 		self.figure = Shape()
 		for block in self.figure.shape:
 			self.grey_rect.add_child(block)
-		global NEW
-		NEW = False
+			global new
+			new = False
 			
 			
 
